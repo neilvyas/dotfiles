8af20f4dcecbe2d6a446e42b95596054b22e81ad
Neil Vyas <nvy@yelp.com>
2016-05-25 10:34:29 -0700

    removing school stuff

--- aliases.sh
+++ aliases.sh
@@ -1,20 +1,7 @@
-alias sl="pmset sleepnow"
-
 #python
 alias py="python" 
 #incredible interpreter.
 alias bp="bpython"
-
-#UT stuff.
-function dir() { finger $1@directory.utexas.edu }
-#school stuff specifically
-CDIR=~/Classes
-alias pde="cd $CDIR/M372K"
-alias eeml="cd $CDIR/EE381V_ML"
-alias aas="cd $CDIR/AAS325"
-alias real="cd $CDIR/M365C"
-alias jazz="cd $CDIR/MUS307"
-alias classes="echo 'pde eeml aas real jazz'"
 
 #git stuff.
 alias gbv="git log --oneline --abbrev-commit --all --graph --decorate --color" 
