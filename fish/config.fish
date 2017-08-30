56f172d373e8f86606b0c41661cd2032a71858f6
Neil Vyas <neil.vyas@affirm.com>
2017-08-29 19:04:55 -0700

    vf support +  sensitive env var path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,13 @@
 set -g DOTFILES_HOME ~/dotfiles
 
+set -gx TERM "xterm-256color"
+
+if pip show virtualfish > /dev/null
+    eval (python -m virtualfish)
+end
+
 source $DOTFILES_HOME/fish/aliases.fish
 source $DOTFILES_HOME/fish/pyenv.fish
+source $DOTFILES_HOME/fish/sensitive.fish
 
 eval $DOTFILES_HOME/fish/nix_fish_env 2>/dev/null
